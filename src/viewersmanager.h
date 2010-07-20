efb881d30d6e192608c86dac932f6da829dec9d0
raphamontana@gmail.com <raphamontana@gmail.com@ab3d61f5-4d71-8516-dab4-e2691a3f895d>
2010-07-20 20:37:29 +0000

    Removed old files: strategymanager.h and viewermanager.h. Added viewersmanager.h file.

---
+++ src/viewersmanager.h
@@ -0,0 +1,57 @@
+/*
+    Copyright (C) 2010  Doi, Montanari, Silva
+
+    This file is part of the USPDroids Football Simulator.
+
+    The USPDroids Football Simulator is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+*/
+
+#ifndef VIEWERS_MANAGER_H
+#define VIEWERS_MANAGER_H
+
+#include "worldmodel.h"
+#include <QMutex>
+#include <QtNetwork>
+#include <QVector>
+
+class ViewerManager : QObject
+{
+
+    Q_OBJECT
+
+public:
+
+    ViewerManager(WorldModel * wm, quint16 viewerPort);
+
+    void initialize();
+
+    void recvCommands();
+
+    void transmitData();
+
+    void finalize();
+
+private slots:
+
+    void receiveMessage();
+
+private:
+
+    WorldModel * wm;
+
+    quint16 viewerPort;
+
+    QUdpSocket listenerSocket;
+
+    QUdpSocket senderSocket;
+
+    QMutex mutex; //Protect the address list
+
+    QVector< QPair<QHostAddress, quint16> > addresses;
+
+};
+
+#endif // VIEWERS_MANAGER_H
